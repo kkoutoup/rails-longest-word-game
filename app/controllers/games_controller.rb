0fff2bc882faa3ac2944e95b8d69aa1c1a928739
Kostas Koutoupis <kkoutoup@gmail.com>
2020-07-11 21:08:48 +0100

    Check for English work working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,27 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = ("a".."z").to_a.sample(10)
   end
 
   def score
+    @answer = params[:answer]
+    @letters = params[:letters]
+    @english_word = english_word?(@answer)
+  end
+
+  private
+
+  def check_answer
+    @answer.split("") - @letters.split("")
+  end
+
+  def english_word?(input)
+    url = "https://wagon-dictionary.herokuapp.com/#{input}"
+    json_string = open(url).read
+    response = JSON.parse(json_string)
+    response["found"]
   end
 end
